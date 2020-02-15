3ecccbb6a2a0d9f8b175d92e9a1ce5838753e6a9
hzmdschhc <275941439@qq.com>
2020-02-16 00:05:02 +0800

    feat: add unit test

---
+++ test.py
@@ -0,0 +1,24 @@
+# coding=utf-8
+
+import unittest
+from ddt import ddt, data, unpack
+import zh2digit
+
+cases = [
+    {'zh': "有十五个月，七天，十周", 'result': ['有', 15, '个月，', 7, '天，', 10, '周']},
+    {'zh': "有一千零十五个月", 'result': ['有', 1015, '个月']},
+    {'zh': '二十三个', 'result': [23, '个']},
+]
+
+@ddt
+class TestZh2Digit(unittest.TestCase):
+
+    @data(*cases)
+    @unpack
+    def test(self, zh, result):
+        res = zh2digit.to_digit(zh)
+        self.assertEqual(res, result)
+
+
+if __name__ == '__main__':
+    unittest.main()
